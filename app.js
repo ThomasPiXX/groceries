3d3be809d2b29bd324929b90099ebdbe7a7e474d
Tom Pivin <tommy.pivin94@gmail.com>
2023-07-11 15:35:36 -0400

    Learning

--- app.js
+++ app.js
@@ -1,12 +1,48 @@
-//////////////////////////////////
-//sqlite connection 
-const sqlite3 = require('sqlite3');
-const db = new sqlite3.Database('');
 /////////////////////////////////
 // express
 const express = require('express');
 const app = express();
 const port = 3000;
 
-
+//ejs
+// Set view engine
+app.set('view engine', 'ejs');
+const path = require('path');
+app.set('views', path.join(__dirname, 'view'));
+//
 app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//////////////////////////////////////
+//start the app/server listening upcoming request
+//start the app/server listening upcoming request
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`)
+});
