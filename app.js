acd8fb404b2e874bc316971d791a3911f58eb37e
Tom Pivin <tommy.pivin94@gmail.com>
2023-07-23 20:47:52 -0400

    password hasher

--- app.js
+++ app.js
@@ -99,7 +99,25 @@ passport.use(new localStrategy((username, password, done) => {
   });
 }));
 //function for hashing password for DB
+function passwordHasher(password, callback){
 
+  if (!password) {
+      const error = new Error('Invalid password');
+      return callback(error);
+  }
+  //Generate a salt to use for hashing
+  bcrypt.genSalt(10, (err, salt) =>{
+      if (err) return callback(err);
+
+      //hash the password using the generated salt 
+      bcrypt.hash(password, salt, (err, hash) => {
+          if (err) return callback(err);
+
+          // invoke the callback
+          callback(null, hash);
+      })
+  })
+}
 
 
 
